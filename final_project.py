c3e3aa2c39bc4ca9ff0bc4b250a576787c7c06ae
Solian21 <soliancarlos@gmail.com>
2024-11-18 17:57:49 -0800

    Basic Page

--- final_project.py
+++ final_project.py
@@ -1,12 +1,12 @@
 # Title; Final Project - CST205
 # Author: Carlos Solian, Aryll Pacheco, Bryson Ruck, & Sarah Wafa
 
-from flask import Flask, render_templates
-from flask_bootstrp import Bootstrap5
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 boostrp = Bootstrap5(app)
 
 @app.route('/')
 def homepage():
-    return render_templates('homepage.html')
+    return render_template('homepage.html')
